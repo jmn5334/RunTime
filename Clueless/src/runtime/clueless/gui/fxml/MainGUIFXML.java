370ce61290939762c70a406c6b6c2034c33676c4
Jacob Newcomer <jmnewc4@gmail.com>
2017-05-01 16:29:30 -0400

    Updated gui to be network configurable.

--- Clueless/src/runtime/clueless/gui/fxml/MainGUIFXML.java
+++ Clueless/src/runtime/clueless/gui/fxml/MainGUIFXML.java
@@ -21,69 +21,19 @@ public class MainGUIFXML {
     // used by the logger to output status to the gui
     @FXML private WebView status_webview;
 
-    @FXML Button moveleft_button;
-    @FXML Button moveright_button;
-    @FXML Button moveup_button;
-    @FXML Button movedown_button;
-    @FXML Button movediagonal_button;
-    @FXML private javafx.scene.control.Button leavegame_button;
-
-
-    @FXML ImageView gameboard_imageview;
-
-    @FXML Button p1_chip;
-    @FXML Button p2_chip;
-    @FXML Button p3_chip;
-    @FXML Button p4_chip;
-    @FXML Button p5_chip;
-    @FXML Button p6_chip;
-
     @FXML ComboBox suggestion_person_combobox;
     @FXML ComboBox suggestion_weapon_combobox;
 
-    @FXML ComboBox chooseplayer_combobox2;
-
-    String playerid;
-    Button playerChip = p1_chip;
-
     @FXML
     public void initialize(){
-        playerid = Config.ptr().str("playernumber");
-
-        if(playerid.contains("p1")){
-            playerChip = p1_chip;
-        }
-
-        gameboard_imageview.setImage(new Image("/icons/gameboard.png"));
 
         GameManager gm = GameManager.getInstance();
-
-        Button[] playerChipsList = {null, p1_chip, p2_chip,p3_chip,p4_chip,p5_chip,p6_chip};
-        gm.setPlayerChipArray(playerChipsList);
-
-        refreshMovementButtons();
 
 
         ArrayList<String> playerchoicelist = new ArrayList<>();
         for(int n=1; n<7; n++)
             playerchoicelist.add(String.valueOf(n));
         ObservableList<String> plist = FXCollections.observableArrayList(playerchoicelist);
-        chooseplayer_combobox2.setItems(plist);
-
-        chooseplayer_combobox2.getSelectionModel().selectedItemProperty().addListener( new ChangeListener(){
-
-            @Override
-            public void changed(ObservableValue observable, Object oldValue, Object newValue){
-
-                if(newValue!=null){
-                    int pid = Integer.valueOf((String)newValue);
-                    GameManager.getInstance().chooseCurrentPlayer(pid);
-
-                    refreshMovementButtons();
-                }
-            }
-
-        });
 
 
         initSuggestOptions();
@@ -129,57 +79,6 @@ public class MainGUIFXML {
         ObservableList<ListNode> plist3 = FXCollections.observableArrayList(list3);
         accuse_weapon_combobox.setItems(plist3);
         suggestion_weapon_combobox.setItems(plist3);
-    }
-
-
-    void refreshMovementButtons(){
-        GameManager gm = GameManager.getInstance();
-        if(gm.canMoveAbove())
-            moveup_button.setDisable(false);
-        else
-            moveup_button.setDisable(true);
-
-        if(gm.canMoveBelow())
-            movedown_button.setDisable(false);
-        else
-            movedown_button.setDisable(true);
-        if(gm.canMoveLeft())
-            moveleft_button.setDisable(false);
-        else
-            moveleft_button.setDisable(true);
-        if(gm.canMoveRight())
-            moveright_button.setDisable(false);
-        else
-            moveright_button.setDisable(true);
-        if (gm.canMoveDiagonal())
-            movediagonal_button.setDisable(false);
-        else
-            movediagonal_button.setDisable(true);
-    }
-
-    @FXML void moveleftCallback(){
-
-        GameManager.getInstance().moveLeft();refreshMovementButtons();
-    }
-    
-    @FXML void moverightCallback(){
-
-        GameManager.getInstance().moveRight();refreshMovementButtons();
-    }
-    @FXML void moveupCallback(){
-
-        GameManager.getInstance().moveUp();refreshMovementButtons();
-    }
-    @FXML void movedownCallback(){
-
-      GameManager.getInstance().moveDown();refreshMovementButtons();
-    }
-    @FXML void movediagonalCallback(){
-        GameManager.getInstance().moveDiagonal();refreshMovementButtons();
-
-    }
-    @FXML public void leavegameCallback(){
-
     }
 
     @FXML public void playerSuggestMurderer(){
